f61994db7eb9f3d8900af9d574bbfd2449787452
matths2 <mattias.holmgren@tietoevry.com>
2025-01-06 00:57:53 +0100

    update ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="trello-csv",
-    version="1.0.5",
+    version="1.0.6",
     description="CLI tool to export Trello board data to a CSV file.",
     long_description=long_description,
     long_description_content_type="text/markdown",
